f2c13640d0ede5b24f9b3184db51cfbcac9a8b87
JCMoure <juancarlos.moure@uab.es>
2026-03-26 15:47:13 +0100

    dummy

--- CH1.cpp
+++ CH1.cpp
@@ -170,7 +170,7 @@ unsigned computeCost ( unsigned *V, unsigned Vsize, unsigned *W, unsigned Wsize 
 
   for ( int i=0; i < Vsize; i++ )
     addV[i] = 0;
-
+    
   for ( int i=0; i < Wsize; i++ )
     addW[i] = 0;
 
